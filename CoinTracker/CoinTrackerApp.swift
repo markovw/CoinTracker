26a00af824f48814d4ddd4054f302adf3e2871c8
markovw <randyhelper@gmail.com>
2024-06-04 18:54:48 +0300

    Initial Commit

---
+++ CoinTracker/CoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerApp.swift
+//  CoinTracker
+//
+//  Created by Vladislav on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MarketView()
+        }
+    }
+}
