e26eb1a0c529c541753e68440254fdac1abc5292
markovw <randyhelper@gmail.com>
2024-07-03 16:13:24 +0300

    Revert "Some improvements"
    This reverts commit c6bda3841ad71f1198924ba5a8289d2a10b51727.

---
+++ CoinTracker/Ext/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  CoinTracker
+//
+//  Created by Vladislav on 03.07.2024.
+//
+
+import SwiftUI
+
+class APIKeys {
+    let key = "CG-rAy6R4nQzYdy8TADwH6mjqUF"
+}
---
+++ CoinTracker/Model/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  TickerRow.swift
+//  CoinTracker
+//
+//  Created by Vladislav on 17.06.2024.
+//
+
+import SwiftUI
+
+class APIKeys {
+    let key = "CG-rAy6R4nQzYdy8TADwH6mjqUF"
+}
